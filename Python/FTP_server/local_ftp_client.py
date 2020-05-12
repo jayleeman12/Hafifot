fb514366d2cd74293626fd32939d648935c81551
Yuval <yuvi9313@gmail.com>
2020-05-12 17:18:21 +0300

    Create a ftp client

---
+++ Python/FTP_server/local_ftp_client.py
@@ -0,0 +1,21 @@
+from os import path as p
+from core.file_controller import FileController
+
+
+class LocalFtpClient:
+    def __init__(self, controller: FileController):
+        self.file_controller = controller
+
+    def save_file(self, file_path, filename):
+        print(p.isfile(file_path))
+
+        with open(file_path, 'rb') as f:
+            data = f.read()
+
+        self.file_controller.save_file(filename, data)
+
+    def get(self, path):
+        if p.isdir(path):
+            self.file_controller.get_all_files(path)
+        else:
+            self.file_controller.get_file(path)
