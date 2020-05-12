080229e7be3d56b32e9c13811fd34eb717b80e23
Yuval <yuvi9313@gmail.com>
2020-05-12 17:33:50 +0300

    Add typings to the function signitures

--- Python/FTP_server/local_ftp_client.py
+++ Python/FTP_server/local_ftp_client.py
@@ -6,13 +6,13 @@ class LocalFtpClient:
     def __init__(self, controller: FileController):
         self.file_controller = controller
 
-    def save_file(self, file_path, filename):
+    def save_file(self, file_path: str, filename: str):
         with open(file_path, 'rb') as f:
             data = f.read()
 
         self.file_controller.save_file(filename, data)
 
-    def get(self, path):
+    def get(self, path: str):
         if p.isdir(path):
             self.file_controller.get_all_files(path)
         else:
