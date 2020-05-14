04fc7e0791895874e708101166f64340b5d576bf
Yuval <yuvi9313@gmail.com>
2020-05-14 14:11:03 +0300

    Add flask logger support

--- Python/FTP_server/app.py
+++ Python/FTP_server/app.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask
 from flask_restful import Api
 from controllers.Rest import files, files_by_id
@@ -10,12 +11,16 @@ def create_app():
     flask_app = Flask(__name__)
 
     api = Api(flask_app)
-    api.add_resource(files.Files, '/files', resource_class_args=[file_service])
-    api.add_resource(files_by_id.FilesById, '/files/<string:file_id>')
+    kwargs = {
+        'logger': flask_app.logger
+    }
+    api.add_resource(files.Files, '/files', resource_class_args=[file_service], resource_class_kwargs=kwargs)
+    api.add_resource(files_by_id.FilesById, '/files/<string:file_id>', resource_class_args=[file_service], resource_class_kwargs=kwargs)
 
     return flask_app
 
 
 if __name__ == '__main__':
     app = create_app()
+    app.logger.setLevel(logging.WARNING)
     app.run()
